4b9a9ddd0b2f299c07d7c985c3ed44422334052d
kotoko <Robsonpizza219@gmail.com>
2025-05-14 19:30:58 -0300

    Create 5em5.c

---
+++ 5em5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+printf("Digite o numero: ");
+int numero = 0;
+scanf("%i", &numero);
+
+for (int i = 1; i <= 10; i++){
+    printf("%i\n", numero = numero + 5);
+
+}
+
+
+return 0;
+}
